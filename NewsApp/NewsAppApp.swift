d0bfa4867c1129aa014c6dc3df382eeca1c9f63b
Lucia Žužić <lucia.zuzic@decode.agency>
2023-10-26 10:02:26 +0200

    first commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Lucia Žužić on 26.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
